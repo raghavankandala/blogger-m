1b3e107b09f0a0d837a7c3eb6b71526379d3fd07
Raghavan <raghavan@raghavan-Vostro-1520.(none)>
2014-10-17 18:53:16 +0530

    Added show, edit, update and delete actions for Blogs feature. Mapped the root of the application to point to blogs#index url. Added a new layout file and did some code refactoring at the view level.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,8 @@
 class BlogsController < ApplicationController
-	
+
+	#layout :false #=> Turn off layout for this controller and all of its actions
+	#layout 'admin' #=> Use admin layout instead of default application layout for this controller and all of its actions.
+
 	# Through this controller, we should be able to 
 	# 1. Create a Blog entry
 	# 2. Read a Blog entry
@@ -23,6 +26,7 @@ class BlogsController < ApplicationController
 		blog_props = params[:blog]
 		@blog = Blog.new(blog_props)
 		if @blog.save #=> returns true if object is saved otherwise returns false
+			redirect_to blogs_path, :notice => "Your blog entry has been created!"
 			# show create.html.erb file
 		else
 			#=> The form data is not valid, so we will show the same form to the user 
@@ -37,16 +41,35 @@ class BlogsController < ApplicationController
 		@blogs = Blog.order('created_at DESC')
 	end
 
+	def show
+		@blog = Blog.find(params[:id])
+		#render :layout => false # Turn off layout for this action alone
+		render :layout => 'application' # This action alone uses admin layout irrespective of what layout the controller is configured with
+	end
+
 	def edit
+		@blog = Blog.find(params[:id])
 	end
 
 	def update
+		@blog = Blog.find(params[:id])
+		blog_props = params[:blog]
+		if @blog.update_attributes(blog_props) 
+			# render update.html.erb
+			redirect_to blog_path(@blog), :notice => "Your blog entry has been updated!" # Redirect to /blogs/<:id>
+		else
+			render :edit
+		end
 	end
 
-	def show
-	end
 
 	def destroy
+		@blog = Blog.find(params[:id])
+		if @blog.destroy
+			redirect_to blogs_path, :notice => "Your blog entry has been deleted!"
+		else
+			redirect_to blogs_path, :notice => "Your blog entry could not be deleted!"
+		end
 	end
 
 end
